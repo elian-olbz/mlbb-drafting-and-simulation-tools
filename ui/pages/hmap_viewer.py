4643f94ae4b8d24e31ec9a7c642d48a3edb380df
marl <mmoriones7@gmail.com>
2023-10-03 16:20:57 +0800

    added play/pause, file opener. not implemented: h_slider, radio buttons

--- ui/pages/hmap_viewer.py
+++ ui/pages/hmap_viewer.py
@@ -3,10 +3,10 @@ import csv
 import os
 import typing
 import cv2
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QGraphicsItemGroup ,QGraphicsScene, QGraphicsView, QGraphicsPixmapItem, QGraphicsEllipseItem, QHBoxLayout, QWidget
-from PyQt6.QtGui import QPixmap, QImage, QBrush, QPainter, QPainterPath, QColor, QPen
-from PyQt6.QtCore import Qt, QTimer, QRectF, QPointF, pyqtSignal, QThread
-from PyQt6 import QtGui, uic
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QGraphicsItemGroup ,QGraphicsScene, QGraphicsView, QGraphicsPixmapItem, QGraphicsEllipseItem, QHBoxLayout, QWidget, QFileDialog
+from PyQt6.QtGui import QPixmap, QImage, QBrush, QPainter, QPainterPath, QColor, QPen, QIcon
+from PyQt6.QtCore import Qt, QTimer, QRectF, QPointF, pyqtSignal, QThread, QSize
+from PyQt6 import uic
 from ui.rsc_rc import *
 from ui.misc.titlebar import TitleBar
 from run_draft_logic.utils import get_icon, rounded_pixmap, load_names, get_name
@@ -24,37 +24,47 @@ class VideoThread(QThread):
         super().__init__()
         self.video_path = video_path
         self.cap = cv2.VideoCapture(video_path)
-        self.running = True 
+        self.running = True
+        self.paused = False  # Add a paused flag
 
     def run(self):
         while self.running:
-            ret, frame = self.cap.read()
-            if ret:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                height, width, channel = frame.shape
-                bytes_per_line = 3 * width
-                q_image = QImage(frame.data, width, height, bytes_per_line, QImage.Format.Format_RGB888)
-                pixmap = QPixmap.fromImage(q_image)
-                self.frame_ready.emit(pixmap)
-                self.msleep(30)
+            if not self.paused:  # Check if the thread is paused
+                ret, frame = self.cap.read()
+                if ret:
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    height, width, channel = frame.shape
+                    bytes_per_line = 3 * width
+                    q_image = QImage(frame.data, width, height, bytes_per_line, QImage.Format.Format_RGB888)
+                    pixmap = QPixmap.fromImage(q_image)
+                    self.frame_ready.emit(pixmap)
+                    self.msleep(30)
+            else:
+                return
 
     def stop(self):
         self.running = False
         self.wait()
 
+    def pause(self):
+        self.paused = True
+
+    def resume(self):
+        self.paused = False
+
 class RawVideo(QLabel):
-    def __init__(self, qlabel):
+    def __init__(self, video_path, qlabel):
         super().__init__()
+        self.video_path = video_path
         self.qlabel = qlabel
         self.video_thread = None
         self.setup_video_thread()
 
     def setup_video_thread(self):
-        video_path = "C:/Users/Marlon/Desktop/vid/bo2.mp4"
         if self.video_thread:
             self.video_thread.stop()
             self.video_thread = None
-        self.video_thread = VideoThread(video_path)
+        self.video_thread = VideoThread(self.video_path)
         self.video_thread.frame_ready.connect(self.update_frame)
 
     def update_frame(self, pixmap=None):
@@ -68,7 +78,6 @@ class RawVideo(QLabel):
         if self.video_thread:
             self.video_thread.stop()
             self.video_thread.wait()
-            self.video_thread.deleteLater()
 
 class BackgroundItem(QGraphicsPixmapItem):
     def __init__(self, pix, view):
@@ -99,6 +108,10 @@ class HeatmapViewerWindow(QMainWindow):
         self.menu_width = 120
         self.title_bar = TitleBar(self)
 
+        self.is_running = False
+        self.video_path = None
+        self.csv_path = None
+
         ui_path = os.path.join(script_dir, "hmap_viewer.ui")
         uic.loadUi(ui_path, self)
 
@@ -125,12 +138,13 @@ class HeatmapViewerWindow(QMainWindow):
         background_image = QPixmap('icons/map.png')
         self.background_item = BackgroundItem(background_image, self.graphics_view)
 
-        self.video = RawVideo(self.video_label)
+        self.video = None
+        self.open_vid_btn.clicked.connect(self.open_video_file)
+        self.open_csv_btn.clicked.connect(self.open_csv_file)
 
         # Add the background item to the scene
         self.scene.addItem(self.background_item)
 
-        self.dataset_filename = "outputs/new.csv"  # Replace with your dataset file's path
         self.data = None
         self.hero_names = load_names("data/hero_map.csv")
         self.class_names = []
@@ -140,17 +154,13 @@ class HeatmapViewerWindow(QMainWindow):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_objects)
         self.play_btn.clicked.connect(self.play_pause)
-
-        self.load_classes()
-        self.draw_objects_from_dataset()
-        self.update_btn_texts()
         self.object_group = QGraphicsItemGroup()  # Create a group for the objects
 
         # Add the object group to the scene
         self.scene.addItem(self.object_group)
 
         self.resizeEvent = self.resize_event
-        self.closeEvent = self.cleanup
+        self.closeEvent = self.cleanup_event
 
         def moveWindow(event):
             if self.title_bar.returnStatus() == True:
@@ -175,58 +185,129 @@ class HeatmapViewerWindow(QMainWindow):
     def mousePressEvent(self, event):
         self.dragPos = event.globalPosition().toPoint()
 
-    def cleanup(self, event):
-        # Stop the video thread and wait for it to finish
+    def clean(self):
         if self.video and self.video.video_thread:
             self.video.video_thread.stop()
             self.video.video_thread.wait()
-            self.video.video_thread.deleteLater()
+
+    def cleanup_event(self, event):
+        # Stop the video thread and wait for it to finish
+        self.clean()
         event.accept()
     
     def resize_event(self, event):
-        super().resizeEvent(event)  # Call the base class method to handle the resizing
-        # Update the scene rectangle to match the new size
+        super().resizeEvent(event)  
         self.scene.setSceneRect(0, 0, self.graphics_view.width(), self.graphics_view.height())
-        # Recalculate the icon size and update the background item scale factor
         self.set_icon_offset()
         self.background_item.set_scale_factor()
         self.load_classes()
+        self.display_first_frame()
 
     def set_icon_offset(self):
         self.ICON_OFFSET = int((75 / ORIGINAL_SIZE) * min(self.graphics_view.height(), self.graphics_view.width()))
     
     def play_pause(self):
-        self.video.video_thread.start()
-        self.timer.start(DELAY)  # Adjust the timer interval (milliseconds) 31
+        if self.csv_path is not None and self.video_path is not None:
+            if not self.is_running:
+                self.is_running = True
+                if not self.video.video_thread.isRunning():
+                    # Start the video thread only if it's not already running
+                    self.video.video_thread.start()
+                self.video.video_thread.resume()  # Resume the video thread
+                self.timer.start(DELAY)  # Adjust the timer interval
+                self.play_btn.setIcon(QIcon('icons/pause.svg'))
+                self.play_btn.setIconSize(QSize(20, 20))
+            else:
+                self.is_running = False
+                self.video.video_thread.pause()  # Pause the video thread
+                self.timer.stop()
+                self.play_btn.setIcon(QIcon('icons/play.svg'))
+                self.play_btn.setIconSize(QSize(20, 20))
+
+    def update_file_name_label(self, path, qlabel):
+        file_name = os.path.basename(path)
+        qlabel.setText(f"{file_name}")
+        qlabel.setFixedWidth(120)
+
+    def open_video_file(self):
+        if self.is_running == True:
+            return
+        else:
+            video_path, _ = QFileDialog.getOpenFileName(self, "Open Video File", "", "Video Files (*.mp4 *.avi *.mkv)")
+            if video_path:
+                if self.is_running:
+                    self.stop_video()
+                
+                self.video_path = video_path
+                self.video = RawVideo(self.video_path, self.video_label)
+                self.update_file_name_label(self.video_path, self.vid_name)
+                self.display_first_frame()
+            else:
+                return
+
+    def open_csv_file(self):
+        if self.is_running == True:
+            return
+        else:
+            csv_path, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv)")
+            if csv_path:
+                self.csv_path = csv_path
+                self.draw_objects_from_dataset()
+                self.load_classes()
+                self.update_btn_texts()
+                self.update_file_name_label(self.csv_path, self.csv_name)
+            else:
+                return
+            
+    def display_first_frame(self):
+        if not self.is_running:
+            if self.video_path:
+                cap = cv2.VideoCapture(self.video_path)
+                ret, frame = cap.read()
+                cap.release()
+
+                if ret:
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    height, width, channel = frame.shape
+                    bytes_per_line = 3 * width
+                    q_image = QImage(frame.data, width, height, bytes_per_line, QImage.Format.Format_RGB888)
+                    pixmap = QPixmap.fromImage(q_image)
+
+                    # Scale the pixmap to fit the size of self.video_label
+                    pixmap = pixmap.scaled(self.video_label.size(), Qt.AspectRatioMode.KeepAspectRatio)
+                    self.video_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                    self.video_label.setPixmap(pixmap)
 
 
     def load_classes(self):
         class_ids = set()
-        
-        with open(self.dataset_filename, "r") as file:
-            csv_reader = csv.reader(file)
-            next(csv_reader)  # Skip the header row
 
-            for row in csv_reader:
-                obj_class = int(row[2])
-                class_ids.add(obj_class)
+        if self.csv_path is not None:
+            with open(self.csv_path, "r") as file:
+                csv_reader = csv.reader(file)
+                next(csv_reader)  # Skip the header row
 
-        for class_id in class_ids:
-            image_path = get_icon(class_id + 1) # image files are named after the class names
-            pixmap = QPixmap(image_path)
-            scaled_pix = pixmap.scaled(self.ICON_OFFSET, self.ICON_OFFSET, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
-            r_pix = rounded_pixmap(scaled_pix, self.ICON_OFFSET, 1)
-            self.class_images[class_id] = r_pix
+                for row in csv_reader:
+                    obj_class = int(row[2])
+                    class_ids.add(obj_class)
 
-        id_list = list(self.class_images.keys())
-        for item in id_list:
-            self.class_names.append(get_name(item + 1, self.hero_names))
+            for class_id in class_ids:
+                image_path = get_icon(class_id + 1) # image files are named after the class names
+                pixmap = QPixmap(image_path)
+                scaled_pix = pixmap.scaled(self.ICON_OFFSET, self.ICON_OFFSET, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+                r_pix = rounded_pixmap(scaled_pix, self.ICON_OFFSET, 1)
+                self.class_images[class_id] = r_pix
+
+            id_list = list(self.class_images.keys())
+            for item in id_list:
+                self.class_names.append(get_name(item + 1, self.hero_names))
 
     def draw_objects_from_dataset(self):
-        with open(self.dataset_filename, "r") as file:
-            csv_reader = csv.reader(file)
-            next(csv_reader)  # Skip the header row
-            self.data = list(csv_reader)
+        if self.csv_path is not None:
+            with open(self.csv_path, "r") as file:
+                csv_reader = csv.reader(file)
+                next(csv_reader)  # Skip the header row
+                self.data = list(csv_reader)
 
     def update_btn_texts(self):
         if self.class_names is not None:
